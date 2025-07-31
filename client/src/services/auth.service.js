19ad28019760d5d4402d1c92bd1a43e876683e46
BMSITCSEE <156016070+BMSITCSEE@users.noreply.github.com>
2025-08-01 02:40:26 +0530

    Create auth.service.js

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,50 @@
+import api from './api';
+
+export const authService = {
+  login: async (credentials) => {
+    const { data } = await api.post('/auth/login', credentials);
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+    }
+    return data;
+  },
+
+  register: async (userData) => {
+    const { data } = await api.post('/auth/register', userData);
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+    }
+    return data;
+  },
+
+  logout: async () => {
+    await api.post('/auth/logout');
+    localStorage.removeItem('token');
+  },
+
+  getMe: async () => {
+    const { data } = await api.get('/auth/me');
+    return data;
+  },
+
+  updateProfile: async (profileData) => {
+    const { data } = await api.put('/auth/update-profile', profileData);
+    return data;
+  },
+
+  updatePassword: async (passwordData) => {
+    const { data } = await api.put('/auth/update-password', passwordData);
+    return data;
+  },
+
+  forgotPassword: async (email) => {
+    const { data } = await api.post('/auth/forgot-password', { email });
+    return data;
+  },
+
+  resetPassword: async (token, password) => {
+    const resetUrl = '/auth/reset-password/' + token;
+    const { data } = await api.post(resetUrl, { password });
+    return data;
+  },
+};
